b91c25f169495fbb1f2a0842f6a91ad44e658822
Gaye <gayegul@gmail.com>
2016-07-22 14:15:15 -0700

    initial setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeupStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
